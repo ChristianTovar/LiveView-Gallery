67b8bc363b595a73430df2bce611f1c2a3c02cc7
ChristianTovar <tovarchristian21@gmail.com>
2020-02-19 14:31:56 -0500

    added thumbnails

--- lib/gallery.ex
+++ lib/gallery.ex
@@ -6,4 +6,42 @@ defmodule Gallery do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  @url "https://images.unsplash.com"
+
+  @ids [
+    "photo-1562971179-4ad6903a7ed6",
+    "photo-1552673597-e3cd6747a996",
+    "photo-1561133036-61a7ed56b424",
+    "photo-1530717449302-271006cdc1bf"
+  ]
+
+  @list_size length(@ids)
+
+  def image_url(image_id, params) do
+    @url
+    |> URI.parse()
+    |> URI.merge(image_id)
+    |> Map.put(:query, URI.encode_query(params))
+    |> URI.to_string()
+  end
+
+  def thumb_url(id),
+    do: image_url(id, %{w: 100, h: 100, fit: "crop"})
+
+  def large_url(id),
+    do: image_url(id, %{h: 500, fit: "crop"})
+
+  def first_id(), do: Enum.at(@ids, 0)
+
+  def image_ids(), do: @ids
+
+  def get_image(index, :large) do
+    real_index = Enum.at(@ids, rem(index, @list_size))
+    large_url(real_index)
+  end
+
+  def get_image(index, :thumb) do
+    real_index = Enum.at(@ids, rem(index, @list_size))
+    thumb_url(real_index)
+  end
 end
--- lib/gallery_web/live/gallery_live.ex
+++ lib/gallery_web/live/gallery_live.ex
@@ -1,14 +1,6 @@
 defmodule GalleryWeb.GalleryLive do
   use Phoenix.LiveView
-
-  @cards_list [
-    "https://images.unsplash.com/photo-1562971179-4ad6903a7ed6?h=500&fit=crop",
-    "https://images.unsplash.com/photo-1552673597-e3cd6747a996?h=500&fit=crop",
-    "https://images.unsplash.com/photo-1561133036-61a7ed56b424?h=500&fit=crop",
-    "https://images.unsplash.com/photo-1530717449302-271006cdc1bf?h=500&fit=crop"
-  ]
-
-  @list_size length(@cards_list)
+  alias Gallery
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket, :index, 0)}
@@ -16,11 +8,20 @@ defmodule GalleryWeb.GalleryLive do
 
   def render(assigns) do
     ~L"""
-    <label>Index: <%= @index %></label>
-    <button phx-click="previous">Previous</button>
-    <button phx-click="next">Next</button>
-
-    <img src="<%= get_image(@index) %>">
+    <div>
+      <center>
+        <button phx-click="previous">Previous</button>
+        <button phx-click="next">Next</button>
+      </center>
+    </div>
+    <div>
+      <center>
+        <%= for id <- Gallery.image_ids() do %>
+          <img src="<%= Gallery.thumb_url(id) %>">
+        <% end %>
+      </center>
+    </div>
+    <img src="<%= Gallery.get_image(@index, :large) %>">
     """
   end
 
@@ -30,9 +31,5 @@ defmodule GalleryWeb.GalleryLive do
 
   def handle_event("next", _, socket) do
     {:noreply, update(socket, :index, &(&1 + 1))}
-  end
-
-  defp get_image(index) do
-    Enum.at(@cards_list, rem(index, @list_size))
   end
 end
