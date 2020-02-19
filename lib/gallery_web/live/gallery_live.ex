3424443b6682fa0b6868c93be943c866d01e0bb6
ChristianTovar <tovarchristian21@gmail.com>
2020-02-19 12:03:57 -0500

    added counter

--- lib/gallery_web/live/gallery_live.ex
+++ lib/gallery_web/live/gallery_live.ex
@@ -2,12 +2,17 @@ defmodule GalleryWeb.GalleryLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :counter, 0)}
   end
 
   def render(assigns) do
     ~L"""
-    <h1>Yiiiiipeeeee!</h1>
+    <label>Counter: <%= @counter %></label>
+    <button phx-click="incr">+</button>
     """
+  end
+
+  def handle_event("incr", _, socket) do
+    {:noreply, update(socket, :counter, &(&1 + 1))}
   end
 end
