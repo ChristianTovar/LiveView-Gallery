eb076f70f989bb27e63de6d02f3b076bd870660b
ChristianTovar <tovarchristian21@gmail.com>
2020-03-01 21:02:20 -0500

    fixed prod url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :gallery, GalleryWeb.Endpoint,
-  url: [host: System.get_env("HOSTNAME") || "localhost", port: 80],
+  url: [host: System.get_env("https://aggressive-paltry-goldfish.gigalixirapp.com/") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
